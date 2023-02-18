5143bcf74e1c9eac75068e6dd956fee329f5adcf
Waddimi Saint Louis <94648767+waka-code@users.noreply.github.com>
2023-02-18 12:08:15 -0500

    add

---
+++ escalera.ts
@@ -0,0 +1,32 @@
+/*function staircase(n: number): string {
+   let signal:string = ""; 
+   let output:string = "";
+   for(let i = 1; i <= n; i++){
+      if(signal === ""){
+        signal += i;
+        console.log(signal)
+      }else{
+        signal += 1
+        output = output + "\n" + "\n" + signal
+      }
+   }
+   return output
+}
+
+console.log(staircase(6))*/
+
+function staircase(n: number): string {
+    let output:string = "";
+    let signal:string = "\n" + "#";
+    for(let i = 1; i <= n; i++){
+       if(output === ""){
+         output += "#";
+       }else{
+         output += "#";
+         signal = signal + "\n" + "\n" + output;
+       }
+    }
+    return signal
+ }
+ 
+ console.log(staircase(8))
