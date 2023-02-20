616af64546c503dc34871f8da5b6eb1bccb12cc1
Waddimi Saint Louis <94648767+waka-code@users.noreply.github.com>
2023-02-19 19:02:45 -0500

    Add

---
+++ romanInt.ts
@@ -0,0 +1,86 @@
+/*function value(s: string) {
+  switch (s) {
+    case "I": {
+      return 1;
+      break;
+    }
+    case "V": {
+      return 5;
+      break;
+    }
+    case "X": {
+      return 10;
+      break;
+    }
+    case "L": {
+      return 50;
+      break;
+    }
+    case "C": {
+      return 100;
+      break;
+    }
+    case "D": {
+      return 500;
+      break;
+    }
+    case "M": {
+      return 1000;
+      break;
+    }
+    default: {
+      return 0;
+    }
+  }
+}
+
+function romanToInt(s: string): number {
+  let result: number = 0;
+  console.log(s);
+  for (let i = 0; i <= s.length; i++) {
+    let firtsPositionString: number | string = value(s.charAt(i));
+    console.log(i);
+    console.log(firtsPositionString);
+    if (i + 1 < s.length) {
+      let restPositionString: number | string = value(s.charAt(i + 1));
+      console.log(restPositionString);
+      if (firtsPositionString >= restPositionString) {
+        result = result + firtsPositionString;
+       console.log(result);
+      } else {
+        result = result + (firtsPositionString - restPositionString);
+       console.log(result);
+        i++;
+       console.log(i);
+      }
+    } else {
+      result = result + firtsPositionString;
+      console.log(result);
+    }
+  }
+  console.log(result);
+  return result;
+}
+
+console.log(romanToInt("MM"));*/
+
+function romanToInt(s: string): number {
+  const romanNumber = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  let num: number = 0;
+  for (let i = 0; i < s.length; i++) {
+    const prev: number = romanNumber[s[i]];
+    const next: number = romanNumber[s[i + 1]];
+    if (prev < next) {
+      num -= prev;
+    } else {
+      num += prev;
+    }
+    // (curr < next) ? (num -= curr) : (num += curr);
+  }
+  return num;
+}
+
+console.log(romanToInt("IV"));
+console.log(romanToInt("VIII"));
+console.log(romanToInt("LXXIX"));
+console.log(romanToInt("MCMXCIV"));
