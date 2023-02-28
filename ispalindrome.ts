aed5d78ba421f99778919f1f3114829f79240ab6
Waddimi Saint Louis <94648767+waka-code@users.noreply.github.com>
2023-02-28 10:24:35 -0500

    add

--- ispalindrome.ts
+++ ispalindrome.ts
@@ -1,7 +1,23 @@
 function isPalindrome(x: number): boolean {
-    let convertedNumber = x.toString();
-    let reverString = convertedNumber.split("").reverse().join("");
-    return reverString === convertedNumber ? true : false
- }
- 
- console.log(isPalindrome(858));
+  let convertedNumber = x.toString();
+  let reverString = convertedNumber.split("").reverse().join("");
+  return reverString === convertedNumber ? true : false;
+}
+console.log(isPalindrome(858));
+
+//2
+function isPalindromes(word) {
+  let left = 0;
+  let right = word.length - 1;
+
+  while (left < right) {
+    if (word[left] !== word[right]) return false;
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+isPalindromes("neuquen"); // true
+isPalindromes("Buenos Aires"); // fa
